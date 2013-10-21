d4740a919c7cb504b7a9f88a78f4a2025381b4ec
andygimma <andy.n.gimma@gmail.com>
2013-10-21 03:20:44 -0400

    Delete term infos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,6 +17,7 @@ class StaticPagesController < ApplicationController
       term_id = term.id
       if not Term.exists?(term_id)
         puts "Term Id Is Gone #{term_id}"
+        Term.destroy(term_id)
       end
 
     end
